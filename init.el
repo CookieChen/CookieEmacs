3988bc5689bea3f683a042aaa838905bebf2dbe0
CookieChen <orangedog227@gmail.com>
2016-12-01 14:51:56 +0800

    Update Emacs Enviroment

---
+++ init.el
@@ -0,0 +1,32 @@
+
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+
+					;FOLDER-BROWSWER
+
+(add-to-list 'load-path "~/.emacs.d/package/neotree/")
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
+
+
+					;DEBUGGING BY SYMBLO
+
+(defun create-tags (dir-name)
+  "Create tags file."
+  (interactive "DDirectory: ")
+  (eshell-command
+   (format "find %s -type f -name \"*.[ch]\" | etags -")))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (manoj-dark))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
